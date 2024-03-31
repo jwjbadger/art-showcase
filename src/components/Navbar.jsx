2bd8dfe7c62b754ca8aa9c40bc200333a08572bf
jwjbadger <jwjbadger@gmail.com>
2024-03-30 21:24:06 -0500

    Theoretically about us page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { CardText, Mortarboard, Palette } from 'react-bootstrap-icons';
+
+const Navbar = (props) => {
+	return (
+		<>
+			<Nav></Nav>
+		</>
+	);
+};
+
+export default Navbar;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,36 @@
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
-
 const AboutUs = (props) => {
 	return (
-        <>
-        </>);
+		<>
+			<div id='BHS-pic-container'>
+				<img
+					src='/bhsabout.jpg'
+					alt='Brentwood High School'
+					id='BHSPic'
+				></img>
+				<h1>About Us</h1>
+			</div>
+			<p>
+				The BHS Art Department offers an extensive program for students
+				of varied interests and abilities. We encourage each student to
+				realize his/her potential through self-expression in the Visual
+				Arts. Additionally, all of our courses are multi-faceted and
+				incorporate art appreciation, historical and cultural relevance
+				as demonstrated through visual experience. Students may choose
+				to pursue a sequential program in Fine Art culminating in AP in
+				which students prepare a portfolio of work used for applying to
+				colleges, or they may enroll in single courses as an elective.
+				Both options help educate the “whole” person and foster creative
+				problem solving and higher order thinking skills.
+			</p>
+			<p>
+				Attention Art Students: Each Art course at BHS has requests for
+				specific supplies. Please explore the Art Supplies List to see
+				what each art student will need for the upcoming school year.
+				Contact your teacher if you have questions concerning the
+				supplies.
+			</p>
+		</>
+	);
 };
 
 export default AboutUs;
