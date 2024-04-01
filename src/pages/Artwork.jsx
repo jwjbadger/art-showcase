e6c5501d62c706059206ccb615a703a517bc0c1c
jwjbadger <jwjbadger@gmail.com>
2024-04-01 10:20:35 -0500

    Finish classes page

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -1,5 +1,16 @@
+import artworks from '../data/projects.json';
+import Masonry from 'react-responsive-masonry';
+
 const Artwork = (props) => {
-	return <>Artwork</>;
+	return (
+		<>
+			<Masonry columnsCount={3} gutter={4}>
+				{artworks.projects.map((artwork) => (
+					<img src={artwork.URL} />
+				))}
+			</Masonry>
+		</>
+	);
 };
 
 export default Artwork;
--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,5 +1,39 @@
+import { Card, Col, Button } from 'react-bootstrap';
+import classes from '../data/classes.json';
+
 const Classes = (props) => {
-	return <>Classes</>;
+	const handleInterest = (e) => {
+		fetch('https://joesph.kreatesomething.dev/api/classes/interest/', {
+			method: 'POST',
+			headers: new Headers(),
+			body: JSON.stringify({
+				class: e.target.parentElement.children[0].innerText,
+			}),
+		})
+			.then((res) => {
+				if (res.status === 200)
+					console.log('SUCCESSFULLY SHOWED INTEREST');
+			})
+			.catch((err) => console.err(err));
+	};
+
+	return (
+		<>
+			<Col>
+				{classes.classes
+					.filter((e) => e.Type === 'Class')
+					.map((clas) => (
+						<Card className='card-class'>
+							<Card.Title>{clas.Name}</Card.Title>
+							<Card.Text>{clas.Description}</Card.Text>
+							<Button variant='primary' onClick={handleInterest}>
+								I'm interested!
+							</Button>
+						</Card>
+					))}
+			</Col>
+		</>
+	);
 };
 
 export default Classes;
